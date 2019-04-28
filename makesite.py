b9ae7c003c1e87758420cb18097ca39a800951b6
Tushar Chandra <tusharchandra2018@u.northwestern.edu>
2019-04-27 19:45:07 -0500

    Remove unused imports

--- makesite.py
+++ makesite.py
@@ -1,68 +1,67 @@
-#!/usr/bin/env python
-
-"""Make static website/blog with Python."""
-
+"""Generate my static website with Python"""
 
 import commonmark
 
+
+from pathlib import Path
+from typing import Dict, Generator, Tuple
+
 import os
 import shutil
-import distutils.dir_util as dir_util
 import re
 import glob
 import sys
-import json
-import datetime
 
 
-def fread(filename):
+def fread(filename: Path) -> str:
     """Read file and close the file."""
+
     with open(filename, "r") as f:
         return f.read()
 
 
-def fwrite(filename, text):
+def fwrite(filename: Path, text: str):
     """Write content to file and close the file."""
-    basedir = os.path.dirname(filename)
-    if not os.path.isdir(basedir):
-        os.makedirs(basedir)
 
+    filename.parent.mkdir(exist_ok=True)
     with open(filename, "w") as f:
         f.write(text)
 
 
-def log(msg, *args):
+def log(msg: str, *args):
     """Log message with specified arguments."""
+
     sys.stderr.write(msg.format(*args) + "\n")
 
 
-def truncate(text, words=25):
+def truncate(text: str, words: int = 25):
     """Remove tags and truncate text to the specified number of words."""
+
     return " ".join(re.sub("(?s)<.*?>", " ", text).split()[:words])
 
 
-def read_headers(text):
-    """Parse headers in text and yield (key, value, end-index) tuples."""
+def read_headers(text: str) -> Generator[Tuple[str, str, int], None, None]:
+    """Parse headers in text and yield (key, value, end-index) tuples.
+
+    Content files can include optional parameters in the form of <!-- key: value -->, so we want
+    to be able to identify and parse these.
+    """
+
     for match in re.finditer(r"\s*<!--\s*(.+?)\s*:\s*(.+?)\s*-->\s*|.+", text):
         if not match.group(1):
             break
         yield match.group(1), match.group(2), match.end()
 
 
-def rfc_2822_format(date_str):
-    """Convert yyyy-mm-dd date string to RFC 2822 format date string."""
-    d = datetime.datetime.strptime(date_str, "%Y-%m-%d")
-    return d.strftime("%a, %d %b %Y %H:%M:%S +0000")
-
-
-def read_content(filename):
+def read_content(filename: Path) -> Dict[str, str]:
     """Read content and metadata from file into a dictionary."""
-    # Read file content.
+
     text = fread(filename)
 
     # Read metadata and save it in a dictionary.
-    date_slug = os.path.basename(filename).split(".")[0]
-    match = re.search(r"^(?:(\d\d\d\d-\d\d-\d\d)-)?(.+)$", date_slug)
+    match = re.search(r"^(?:(\d\d\d\d-\d\d-\d\d)-)?(.+)$", filename.stem)
+    if not match:
+        raise FileNotFoundError
     content = {"date": match.group(1) or "1970-01-01", "slug": match.group(2)}
 
     # Read headers.
@@ -74,17 +73,16 @@ def read_content(filename):
     text = text[end:]
 
     # Convert Markdown content to HTML.
-    if filename.endswith((".md", ".mkd", ".mkdn", ".mdown", ".markdown")):
+    if filename.suffix in (".md", ".mkd", ".mkdn", ".mdown", ".markdown"):
         text = commonmark.commonmark(text)
 
-    # Update the dictionary with content and RFC 2822 date.
-    content.update({"content": text, "rfc_2822_date": rfc_2822_format(content["date"])})
-
+    content.update({"content": text})
     return content
 
 
-def render(template, **params):
+def render(template: str, **params) -> str:
     """Replace placeholders in template with values from params."""
+
     return re.sub(
         r"{{\s*([^}\s]+)\s*}}",
         lambda match: str(params.get(match.group(1), match.group(0))),
@@ -92,20 +90,21 @@ def render(template, **params):
     )
 
 
-def make_pages(src, dst, layout, **params):
-    """Generate pages from page content."""
+def make_pages(src: str, dst: str, layout: str, **params):
+    """Generate pages from page content.
+    
+    dst: optionally renderable string for destination page, i.e., you can include {{ parameters }} in it.
+    """
+
     items = []
 
     for src_path in glob.glob(src):
-        content = read_content(src_path)
-
+        content = read_content(Path(src_path))
         page_params = dict(params, **content)
 
-        # Populate placeholders in content if content-rendering is enabled.
-        if page_params.get("render") == "yes":
-            rendered_content = render(page_params["content"], **page_params)
-            page_params["content"] = rendered_content
-            content["content"] = rendered_content
+        rendered_content = render(page_params["content"], **page_params)
+        page_params["content"] = rendered_content
+        content["content"] = rendered_content
 
         items.append(content)
 
@@ -113,95 +112,31 @@ def make_pages(src, dst, layout, **params):
         output = render(layout, **page_params)
 
         log("Rendering {} => {} ...", src_path, dst_path)
-        fwrite(dst_path, output)
+        fwrite(Path(dst_path), output)
 
     return sorted(items, key=lambda x: x["date"], reverse=True)
 
 
-def make_list(posts, dst, list_layout, item_layout, **params):
-    """Generate list page for a blog."""
-    items = []
-    for post in posts:
-        item_params = dict(params, **post)
-        item_params["summary"] = truncate(post["content"])
-        item = render(item_layout, **item_params)
-        items.append(item)
-
-    params["content"] = "".join(items)
-    dst_path = render(dst, **params)
-    output = render(list_layout, **params)
-
-    log("Rendering list => {} ...", dst_path)
-    fwrite(dst_path, output)
-
-
 def main():
-    # Create a new _site directory from scratch.
-    if os.path.isdir("_site"):
-        shutil.rmtree("_site")
-    shutil.copytree("static", "_site")
+    if Path("./site").is_dir():
+        shutil.rmtree("site")
+
+    shutil.copytree("static", "site")
 
     # Default parameters.
-    params = {
-        "base_path": "",
-        "subtitle": "Lorem Ipsum",
-        "author": "Admin",
+    params: Dict[str, str] = {
+        "base_path": "site",
+        "subtitle": "Tushar Chandra",
+        "author": "Tushar Chandra",
         "site_url": "tusharc.dev",
-        "current_year": datetime.datetime.now().year,
+        "current_year": "2019",
     }
 
-    # If params.json exists, load it.
-    if os.path.isfile("params.json"):
-        params.update(json.loads(fread("params.json")))
+    page_layout = fread(Path("layout/page.html"))
 
-    # Load layouts.
-    page_layout = fread("layout/page.html")
-    post_layout = fread("layout/post.html")
-    list_layout = fread("layout/list.html")
-    item_layout = fread("layout/item.html")
-
-    # Combine layouts to form final layouts.
-    post_layout = render(page_layout, content=post_layout)
-    list_layout = render(page_layout, content=list_layout)
-
-    # Create site pages.
-    make_pages("content/_index.html", "_site/index.html", page_layout, **params)
-    make_pages("content/[!_]*.html", "_site/{{ slug }}/index.html", page_layout, **params)
-
-    # Create blogs.
-    blog_posts = make_pages(
-        "content/blog/*.md", "_site/blog/{{ slug }}/index.html", post_layout, blog="blog", **params
-    )
-    news_posts = make_pages(
-        "content/news/*.html",
-        "_site/news/{{ slug }}/index.html",
-        post_layout,
-        blog="news",
-        **params
-    )
-
-    # Create blog list pages.
-    make_list(
-        blog_posts,
-        "_site/blog/index.html",
-        list_layout,
-        item_layout,
-        blog="blog",
-        title="Blog",
-        **params
-    )
-    make_list(
-        news_posts,
-        "_site/news/index.html",
-        list_layout,
-        item_layout,
-        blog="news",
-        title="News",
-        **params
-    )
-
-    # Copy contents to root directory
-    dir_util.copy_tree("_site", "./", update=1)
+    make_pages("content/[!_]*.html", "site/{{ slug }}.html", page_layout, **params)
+    make_pages("content/[!_]*.md", "site/{{ slug }}.html", page_layout, **params)
+    make_pages("content/spark/*.md", "site/spark/{{ slug }}.html", page_layout, **params)
 
 
 # Test parameter to be set temporarily by unit tests.
